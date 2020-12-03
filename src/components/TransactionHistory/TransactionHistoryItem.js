c6103547d91ea2fe23cb4e4a62bb104d9b4b472b
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-03 23:18:26 +0200

    Create TransactionHistoryItem.js

---
+++ src/components/TransactionHistory/TransactionHistoryItem.js
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+import s from './TransactionHistoryItem.module.css';
+
+export default function TransactionHistoryItem({ type, amount, currency }) {
+  return (
+    <tr className={s.row}>
+      <td className={s.cell}>{type}</td>
+      <td className={s.cell}>{amount}</td>
+      <td className={s.cell}>{currency}</td>
+    </tr>
+  );
+}
+
+TransactionHistoryItem.propTypes = {
+  type: PropTypes.string,
+  amount: PropTypes.string,
+  currency: PropTypes.string,
+};
