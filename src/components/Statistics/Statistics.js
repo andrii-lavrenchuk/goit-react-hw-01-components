240963be70b52daa09e294574c7915e7e9edaeba
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-03 23:18:17 +0200

    Create Statistics.js

---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import getRandomColor from './randomColor';
+import s from './Statistics.module.css';
+
+export default function Statistics({ title, stats }) {
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
+
+      <ul className={s.list}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={s.item}
+            key={id}
+            style={{ backgroundColor: `${getRandomColor()}` }}
+          >
+            <span>{label} </span>
+            <span>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+};
