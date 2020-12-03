8bcafb368ee118a52a05d98bdf96c963d2781293
andrii-lavrenchuk <63540148+andrii-lavrenchuk@users.noreply.github.com>
2020-12-03 23:18:13 +0200

    Create randomColor.js

---
+++ src/components/Statistics/randomColor.js
@@ -0,0 +1,6 @@
+export default function getColorByRandom() {
+  const r = Math.round(Math.random() * 256);
+  const g = Math.round(Math.random() * 256);
+  const b = Math.round(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
